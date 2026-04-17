4bcb23d6d9084de73d556e9ad68e77dca5b6af68
Prince <princechavez870@gmail.com>
2026-04-17 10:28:29 -0500

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,1 +1,12 @@
-print("funciona")
+local allowedUsers = {
+    [TU_USER_ID] = true
+}
+
+local player = game.Players.LocalPlayer
+
+if not player or not allowedUsers[player.UserId] then
+    return warn("Not Authorized")
+end
+
+-- Si pasa la validación carga el real
+loadstring(game:HttpGet("https://cooprince593.github.io/Script-hub/main.lua"))()
